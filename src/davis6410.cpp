2a0cf5bfe3e6ab51e0a1d5a79cf8af7eaa634bbd
phoenix <lazydog66@mac.com>
2021-07-30 19:23:55 +0100

    working speed and direction

---
+++ src/davis6410.cpp
@@ -0,0 +1,106 @@
+#include <Arduino.h>
+#include <math.h>
+
+#include "davis6410.h"
+
+
+using microseconds_t = unsigned long;
+using milliseconds_t = unsigned long;
+
+
+// Debounce period for the wind speed pulses.
+// At 200 mph we have 1 pulse per 11.26 ms. With a realistic max wind speed of
+// 130 mph we can expect a pulse every 17.32 ms. Therefore a debounce period of
+// less than 17 ms should suffice.
+constexpr milliseconds_t k_wind_pulse_debounce = 10;
+
+// The counter for the wind pulses.
+// The anenometer spins at 1600 rev/hrs at 1 mph, or 0.444r pulses per second
+// per 1 mph. For a max speed of 200 mph this works out 88.888r pulses per
+// second or 222.222r pulses in a 2.5 second sampling period. An 8 bit counter
+// should be more than sufficient.
+static volatile uint8_t wind_speed_pulse_counter = 0;
+
+// At 200 mph we have 1 pulse per 11.26 ms. With a realistic max wind speed of
+// 130 mph we can expect a pulse every 17.32 ms. Therefore a debounce period of
+// less than 17 ms should suffice.
+static volatile milliseconds_t debounce_start_t = 0;
+
+// --------------------------------------------------------------------------------------------------------------------
+// The isr for servicing the wind speed reading.
+// The variable debounce_start_t should be cleared before the first interrupt of
+// a sample period.
+// --------------------------------------------------------------------------------------------------------------------
+static void isr_6410() {
+  milliseconds_t now = millis();
+  if (now - debounce_start_t >= k_wind_pulse_debounce) {
+    ++wind_speed_pulse_counter;
+    debounce_start_t = now;
+  }
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+// Constructor does not initialise the hardware.
+// --------------------------------------------------------------------------------------------------------------------
+davis6410::davis6410(int wind_speed_pin, int wind_vane_pin, unsigned long sample_period)
+  : wind_speed_pin_{wind_vane_pin}, wind_vane_pin_{wind_vane_pin}, sample_period_{sample_period}
+{
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+// The isr for servicing the wind speed reading.
+// The variable debounce_start_t should be cleared before the first interrupt of a sample period.
+// --------------------------------------------------------------------------------------------------------------------
+void davis6410::initialise()
+{
+  pinMode(wind_speed_pin_, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(wind_speed_pin_), isr_6410, FALLING);
+
+  initialised_ = true;
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+// Sample the wind speed.
+// Returns the speed in mpp.
+// --------------------------------------------------------------------------------------------------------------------
+float davis6410::sample_wind_mph() const {
+  // Must be intialsied first.
+  if (!initialised_) return 0;
+
+  // Start a new sample frame.
+  wind_speed_pulse_counter = 0;
+
+  // We're only interested in the speed to the nearest mph so using delay()
+  // should be accurate enough.
+  sei(); 
+  delay(k_wind_speed_sample_t);
+  cli();
+  
+  return calculate_wind_mph(wind_speed_pulse_counter);
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+// Sample the wind direction.
+// Returns the direction using 16 directions, 0 is North, 4 is East etc.
+// --------------------------------------------------------------------------------------------------------------------
+int davis6410::sample_wind_direction() const
+{
+  int drn = analogRead(wind_vane_pin_);
+Serial.println(drn);
+  drn = (drn + 31) >> 6;
+
+  return drn;
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+// Convert wind speed pulse count to kph.
+// This is done as a function so that in future, it will be easy to add a look
+// up table for calibrating the anenometer if we think it needs one. For now,
+// though, we assume that the formula V=P(2.25/T) given in the spec is correct.
+// --------------------------------------------------------------------------------------------------------------------
+float davis6410::calculate_wind_mph(uint8_t pulses) const {
+  float v_mph =
+      pulses * 2.25f * 1000.f / static_cast<float>(k_wind_speed_sample_t);
+
+  return v_mph;
+}
---
+++ src/davis6410.h
@@ -0,0 +1,57 @@
+
+// The default pin for the wind sensor.
+// Pin 2 is used because falling edge interrupts are used.
+constexpr int k_wind_sensor_pin = 2;
+
+// The default pin for the wind vane.
+// The pin must be an analogue pin.
+constexpr int k_wind_direction_pin = A0;
+
+// This is the default duration over which the wind speed is calculated.
+// The anenometer's spec says the minimum wind speed is 1 mph which is 1
+// revolution per 2.25 seconds, so 2.25 seconds seems like a reasonable amount
+// of time. This has the advantage that the returned wind speed will be an
+// integer number of 1 mph.
+constexpr unsigned long k_wind_speed_sample_t = 2250;
+
+class davis6410 {
+ public:
+  // The Davis runs off two pins, a digital input for the wind speed pulses and
+  // an analogue pin for the wind direction. The anenometer's spec says the
+  // minimum wind speed is 1 mph which is 1 revolution per 2.25 seconds, so
+  // 2.25 seconds for the period has the advantage that the returned pulse count
+  // is the wind speed in mph.
+  davis6410(int wind_sensor_pin = k_wind_sensor_pin,
+            int wind_direction_pin = k_wind_direction_pin,
+            unsigned long sample_period = 2250);
+
+  // Initialise the hardware resources and set up the isr.
+  // This must be done once before the 6410 can be used.
+  void initialise();
+
+  // Sample the wind speed.
+  // The pulses from the Davis are counted over the sampling period and
+  // converted to mph.
+  float sample_wind_mph() const;
+
+  // Sample the wind direction.
+  // Returns (0, 15) where 0 is North, 4 is East etc.
+  int sample_wind_direction() const;
+
+ private:
+  // Convert pulses to mph.
+  // Note, this may in the future apply calibration data to the result.
+  float calculate_wind_mph(uint8_t pulses) const;
+
+  // A digital pin is used to counting the anenometer pulses.
+  const int wind_speed_pin_;
+
+  // The analog pin to use for reading the wind vane direction.
+  const int wind_vane_pin_;
+
+  // The duration in milliseconds of the sample period.
+  unsigned long sample_period_;
+
+  // The resources must be initialised before the 6410 can be read.
+  bool initialised_ = false;
+};
